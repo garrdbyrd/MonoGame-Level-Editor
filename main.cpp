a3fa49466f61f5a1e69a7054c278629247170e09
Garrett Byrd <gdb070@yahoo.com>
2024-01-08 00:04:26 -0500

    Added icon

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "caspian.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon("/home/blackbox/Documents/gamedev/caspian/caspian/caspian.png")); //fix later
     Caspian w;
     w.show();
     return a.exec();
